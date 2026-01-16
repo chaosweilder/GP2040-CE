a654a92413c5b7e9aced64fbb984ba9c3d07ebd9
chaosweilder <timehero.final.anti.form@gmail.com>
2026-01-16 16:23:30 +0100

    Update LayoutConfig.jsx

--- www/src/Locales/en/LayoutConfig.jsx
+++ www/src/Locales/en/LayoutConfig.jsx
@@ -47,7 +47,7 @@ export default {
 			BUTTON_LAYOUT_BOARD_DEFINED_ALT5_A: 'Board Defined Alternate 5',
 			BUTTON_LAYOUT_BOARD_DEFINED_ALT6_A: 'Board Defined Alternate 6',
 			BUTTON_LAYOUT_BOARD_DEFINED_ALT7_A: 'Board Defined Alternate 7',
-			BUTTON_LAYOUT_WONDERSWAN_A: 'wonderswan',	
+			BUTTON_LAYOUT_WONDERSWAN_A: 'Wonderswan',	
 		},
 		right: {
 			BUTTON_LAYOUT_ARCADE: 'Arcade',
@@ -99,6 +99,7 @@ export default {
 			BUTTON_LAYOUT_BOARD_DEFINED_ALT5_B: 'Board Defined Alternate 5',
 			BUTTON_LAYOUT_BOARD_DEFINED_ALT6_B: 'Board Defined Alternate 6',
 			BUTTON_LAYOUT_BOARD_DEFINED_ALT7_B: 'Board Defined Alternate 7',
+			BUTTON_LAYOUT_WONDERSWAN_B: 'Wonderswan',
 		},
 	},
 };
