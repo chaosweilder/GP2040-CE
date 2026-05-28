52ad3095e90117540af205d12c7c4709cb032c2e
chaosweilder <timehero.final.anti.form@gmail.com>
2026-05-28 17:30:36 +0200

    Update layoutmanager.h

--- headers/layoutmanager.h
+++ headers/layoutmanager.h
@@ -150,6 +150,9 @@ class LayoutManager {
         LayoutList draw6GAWDRight();
         LayoutList draw6GAWDAllButtonRight();
         LayoutList draw6GAWDAllButtonPlusRight();
+				LayoutList drawneogeoaes();
+				LayoutList drawneogeocdold();
+				LayoutList drawneogeocdnew();
 
         LayoutManager::LayoutList getLeftLayout(uint16_t index);
         LayoutManager::LayoutList getRightLayout(uint16_t index);
